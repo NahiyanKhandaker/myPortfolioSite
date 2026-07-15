14cd00d3645f13046fb85da1a6452497820fb356
NahiyanKhandaker <nahiyank09@gmail.com>
2026-07-15 23:20:26 +0100

    fixed home navigation

--- myPortfolioSite/src/components/intro.tsx
+++ myPortfolioSite/src/components/intro.tsx
@@ -2,7 +2,8 @@ const Intro = () => {
   return (
     <section className="intro">
         <h1 className="intro__title">Hi, I'm Nahiyan Khandaker</h1>
-        <p className="intro__description">I'm a Web Developer & Computer Science student at Perth Academy</p>     
+        <p className="intro__description">I'm a Web Developer & Computer Science student at Perth Academy</p>  
+        <a href="https://github.com/NahiyanKhandaker"><button className = "primaryButton">View My Github Profile!</button></a>   
     </section>
     );
 };
