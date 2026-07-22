395873303473a5cc2e0efa4287f81bffd0bfb03c
NahiyanKhandaker <nahiyank09@gmail.com>
2026-07-23 00:28:31 +0100

    added some extra things to projects page

--- myPortfolioSite/src/main.tsx
+++ myPortfolioSite/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/header.tsx'
 import Home from './App.tsx'
 import About from './pages/about.tsx'
+import ProjectPage from './pages/projects.tsx'   
 import Footer from './components/footer.tsx'
 
 import './index.css'
@@ -12,13 +13,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <div className="app-layout">
-        {/* Header is always visible */}
         <Header /> 
-          
-        {/* Routes go here */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<ProjectPage />} />  
         </Routes>
         <Footer/>
       </div>
